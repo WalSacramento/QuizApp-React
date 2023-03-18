e8bd4753a7173f6f8c5351b91aa8022fa050908a
Waldsson Sacramento <waldsson3@gmail.com>
2023-03-18 15:55:37 -0300

    AddQuestionForm finalizado

--- src/components/AddQuestionForm/index.jsx
+++ src/components/AddQuestionForm/index.jsx
@@ -1,46 +1,82 @@
 import React, { useState } from 'react'
-import { Link } from "react-router-dom";
-import { useFormik } from 'formik';
+import { Link } from 'react-router-dom'
+import { useFormik } from 'formik'
 
 import S from './styles.module.scss'
 
-export function AddQuestionForm () {
+export function AddQuestionForm() {
   const formik = useFormik({
-    initialValues: {id: '', question: '', answers: '', correctAnswer: ''},
+    initialValues: { id: '', question: '', answers: '', correctAnswer: '' },
     onSubmit: values => {
       console.log(values)
     }
   })
 
-
-  return (   
+  return (
     <div className={S.container}>
       <div className={S.card}>
         <h1>Adicionar pergunta ao quiz!</h1>
 
         <div>
           <form onSubmit={formik.handleSubmit}>
-            <label>ID</label>
-            <input type="text" name="id" onChange={formik.handleChange} value={formik.values.id}/>
+            <div className={S.questionContainer}>
+              <label>Pergunta:</label>
+              <input
+                type="text"
+                name="question"
+                onChange={formik.handleChange}
+                value={formik.values.question}
+              />
+            </div>
 
-            <label>Pergunta</label>
-            <input type="text" name="question" onChange={formik.handleChange} value={formik.values.question}/>
+            <div className={S.questionContainerAnswers}>
+              <label>Respostas:</label>
+              <input
+                type="text"
+                name="answer1"
+                onChange={formik.handleChange}
+                value={formik.values.answer1}
+              />
+              <input
+                type="text"
+                name="answer2"
+                onChange={formik.handleChange}
+                value={formik.values.answer2}
+              />
+              <input
+                type="text"
+                name="answer3"
+                onChange={formik.handleChange}
+                value={formik.values.answer3}
+              />
+              <input
+                type="text"
+                name="answer4"
+                onChange={formik.handleChange}
+                value={formik.values.answer4}
+              />
+            </div>
 
-            <label>Respostas</label>
-            <input type="text" name="answers" onChange={formik.handleChange} value={formik.values.answers}/>
-            <input type="text" name="answers" onChange={formik.handleChange} value={formik.values.answers}/>
-            <input type="text" name="answers" onChange={formik.handleChange} value={formik.values.answers}/>
-            <input type="text" name="answers" onChange={formik.handleChange} value={formik.values.answers}/>
+            <div className={S.questionContainer}>
+              <label>Resposta correta:</label>
+              <input
+                type="text"
+                name="correctAnswer"
+                onChange={formik.handleChange}
+                value={formik.values.correctAnswer}
+              />
+            </div>
 
-            <label>Resposta correta</label>
-            <input type="text" name="correctAnswer" onChange={formik.handleChange} value={formik.values.correctAnswer}/>
+            <div className={S.buttons}>
+              <button type="submit">Enviar</button>
 
-            <button type="submit">Enviar</button>
+              <Link to="/">
+                <button className={S.navigationBtn}>Retornar ao início</button>
+              </Link>
+            </div>
           </form>
-          
         </div>
       </div>
     </div>
   )
-
 }
