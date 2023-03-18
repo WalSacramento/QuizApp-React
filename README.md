e670615a5224c09f5bdaf2600b011a4460e7e54b
Waldsson Sacramento <waldsson3@gmail.com>
2023-03-18 16:39:23 -0300

    Adicionado README.md, feitas alterações nas perguntas.

---
+++ README.md
@@ -0,0 +1,33 @@
+#Quiz React
+
+---
+
+Este é um projeto de um Quiz em React.js que utiliza o React Router DOM para navegação entre as páginas e o Formik para criação de um formulário para adicionar novas perguntas ao quiz. O projeto foi criado utilizando o Vite como bundler e pré-processador Sass para estilização das páginas.
+
+##Funcionalidades
+
+- Barra de progresso das questões
+- Demonstração visual sobre o resultado da questão
+- Resultado do quiz ao final
+- Página para adicionar novas perguntas
+- Tecnologias utilizadas
+- React.js
+- Vite
+- Sass
+- React Router DOM
+- Formik
+- GIT/GitHub
+
+##Executando o projeto
+Para executar o projeto é necessário ter o Node.js instalado em sua máquina. Em seguida, siga os passos abaixo:
+
+- Clone o repositório em sua máquina
+- Abra o terminal na pasta raiz do projeto e execute o comando npm install
+- Em seguida, execute o comando npm run dev
+- Abra o navegador e acesse o localhost configurado para visualizar o projeto.
+- Adicionando novas perguntas
+- Para adicionar novas perguntas ao quiz, basta acessar a página "Adicionar Perguntas" no menu de navegação e preencher o formulário com a nova pergunta e suas quatro respostas (sendo uma delas a correta).
+
+##Autor
+###Desenvolvido por **Waldsson Sacramento**.
+###Acesse meu linkedIn : [linkedin.com/in/WaldssonSacramento](https://www.linkedin.com/in/WaldssonSacramento)
--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -1,54 +1,78 @@
-import React, {useState} from 'react'
-import { Link } from "react-router-dom";
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import { QuestionAnswer } from '../QuestionAnswer'
 import { Result } from '../Result'
-import { ProgressBar } from '../ProgressBar';
+import { ProgressBar } from '../ProgressBar'
 
 import S from './styles.module.scss'
 
 const QUESTIONS = [
   {
     id: 1,
-    question: 'Qual é o meu nome?',
-    answers: ['Miguel', 'Luis', 'Matheus', 'Waldsson'],
-    correctAnswer: 'Waldsson'
+    question: 'Qual é a função do método map() em JavaScript?',
+    answers: [
+      'Ele itera sobre um array e retorna um novo array com os resultados da função fornecida.',
+      'Ele remove o último item de um array.',
+      'Ele adiciona um novo item ao final de um array.',
+      'Ele inverte a ordem dos elementos em um array.'
+    ],
+    correctAnswer:
+      'Ele itera sobre um array e retorna um novo array com os resultados da função fornecida.'
   },
   {
     id: 2,
-    question: 'Qual é a minha idade?',
-    answers: ['18', '22', '32', '20'],
-    correctAnswer: '22'
+    question: 'Qual é o propósito do React.js?',
+    answers: [
+      'O React é um banco de dados relacional.',
+      'O React é uma linguagem de programação.',
+      'O React é um framework para desenvolvimento de aplicativos móveis.',
+      'O React é uma biblioteca JavaScript para criar interfaces de usuário.'
+    ],
+    correctAnswer:
+      'O React é uma biblioteca JavaScript para criar interfaces de usuário.'
   },
   {
-    id: 3,
-    question: 'O que sou?',
-    answers: ['Desenvolvedor', 'Médico', 'Eletricista', 'Jogador de Futebol'],
-    correctAnswer: 'Desenvolvedor'
+    id: '3',
+    question: 'O que é uma closure em JavaScript?',
+    answers: [
+      'É um objeto que contém propriedades e métodos.',
+      'É uma função que tem acesso a variáveis ​​em seu escopo léxico, mesmo quando a função é executada fora desse escopo.',
+      'É um elemento HTML que pode ser arrastado e solto.',
+      'É uma função que é executada em um loop infinito.'
+    ],
+    correctAnswer:
+      'É uma função que tem acesso a variáveis ​​em seu escopo léxico, mesmo quando a função é executada fora desse escopo.'
   },
   {
     id: 4,
-    question: 'Onde moro?',
-    answers: ['Penedo', 'Feliz Deserto', 'Recife', ' Maceió'],
-    correctAnswer: 'Penedo'
-  },
-
+    question: 'O que é um componente em React?',
+    answers: [
+      'Um componente é uma função que inverte a ordem dos elementos em um array.',
+      'Um componente é um objeto que contém propriedades e métodos.',
+      'Um componente é uma peça reutilizável de código que retorna elementos React.',
+      ' Um componente é um objeto que contém propriedades e métodos.'
+    ],
+    correctAnswer:
+      'Um componente é uma peça reutilizável de código que retorna elementos React.'
+  }
 ]
 
-export function Quiz () {
+export function Quiz() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
   const [isTakingQuiz, setIsTakingQuiz] = useState(true)
-  const [isCurrentQuestionAnswered, setIsCurrentQuestionAnswered] = useState(false)
+  const [isCurrentQuestionAnswered, setIsCurrentQuestionAnswered] =
+    useState(false)
   const currentQuestion = QUESTIONS[currentQuestionIndex]
   const [correctAnswerCount, setCorrectAnswerCount] = useState(0)
 
   const handleNextQuestion = () => {
-    if (currentQuestionNumber < QUESTIONS.length){
-    setCurrentQuestionIndex(currentQuestionNumber)
-  } else {
-    setIsTakingQuiz(false)
-  }
-  setIsCurrentQuestionAnswered(false)
+    if (currentQuestionNumber < QUESTIONS.length) {
+      setCurrentQuestionIndex(currentQuestionNumber)
+    } else {
+      setIsTakingQuiz(false)
+    }
+    setIsCurrentQuestionAnswered(false)
   }
 
   const handleAnswerQuestion = (event, question, userAnswer) => {
@@ -61,7 +85,7 @@ export function Quiz () {
     const resultClassName = isCorrectAnswer ? S.correct : S.incorrect
 
     event.currentTarget.classList.toggle(resultClassName)
-    
+
     if (isCorrectAnswer) {
       setCorrectAnswerCount(correctAnswerCount + 1)
     }
@@ -76,55 +100,52 @@ export function Quiz () {
   }
 
   const quizSize = QUESTIONS.length
-  const navigationButtonText = currentQuestionIndex + 1 === quizSize ? 'Ver Resultado' : 'Próxima Pergunta';
+  const navigationButtonText =
+    currentQuestionIndex + 1 === quizSize ? 'Ver Resultado' : 'Próxima Pergunta'
   const currentQuestionNumber = currentQuestionIndex + 1
 
   return (
-    
     <div className={S.container}>
       <div className={S.card}>
         {isTakingQuiz ? (
           <div className={S.quiz}>
-            <ProgressBar 
-            size={quizSize} 
-            currentStep={currentQuestionNumber} />
+            <ProgressBar size={quizSize} currentStep={currentQuestionNumber} />
             <header className={S.quizHeader}>
-              <span>PERGUNTA {currentQuestionNumber}/{quizSize} </span>
+              <span>
+                PERGUNTA {currentQuestionNumber}/{quizSize}{' '}
+              </span>
               <p>{currentQuestion.question}</p>
             </header>
-    
+
             <ul className={S.answers}>
               {currentQuestion.answers.map(answer => (
                 <li key={answer}>
-                  <QuestionAnswer 
-                  question={currentQuestion}
-                  answer={answer} 
-                  handleAnswerQuestion={handleAnswerQuestion}/>
+                  <QuestionAnswer
+                    question={currentQuestion}
+                    answer={answer}
+                    handleAnswerQuestion={handleAnswerQuestion}
+                  />
                 </li>
-              ) 
-                )}
+              ))}
             </ul>
-    
-            <div> 
+
+            <div>
               <button className={S.navigationBtn} onClick={handleNextQuestion}>
                 {navigationButtonText}
               </button>
-              
-              <Link to='/'>
+
+              <Link to="/">
                 <button className={S.navigationBtn}>Retornar ao início</button>
               </Link>
             </div>
-
           </div>
         ) : (
-          <Result 
-          correctAnswersCount={correctAnswerCount}
-          quizSize={quizSize}
-          handleTryAgain={handleTryAgain} />
-        )
-        }
-
-        
+          <Result
+            correctAnswersCount={correctAnswerCount}
+            quizSize={quizSize}
+            handleTryAgain={handleTryAgain}
+          />
+        )}
       </div>
     </div>
   )
